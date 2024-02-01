835639b6dc3751fe43e601bc41d172091e0f281c
udonrm <b201395@hiroshima-u.ac.jp>
2024-02-02 04:09:38 +0900

    ユーザー詳細ページの実装

--- src/app/components/userInformation.tsx
+++ src/app/components/userInformation.tsx
@@ -12,18 +12,20 @@ export const UserInformation = () => {
   const { data: session, status } = useSession();
   return (
     <>
-      <Card className="w-[350px]">
-        <CardHeader>
-          <CardTitle>{session?.user?.name}</CardTitle>
-          <CardDescription>{session?.user?.introduction}</CardDescription>
-        </CardHeader>
-        <CardContent className="grid w-full items-center gap-4">
-          <Avatar>
-            <AvatarImage src={session?.user?.image || ""} />
-            <AvatarFallback>CN</AvatarFallback>
-          </Avatar>
-        </CardContent>
-      </Card>
+      <div className="flex justify-center">
+        <Card className="w-[350px]">
+          <CardHeader>
+            <CardTitle>{session?.user?.name}</CardTitle>
+            <CardDescription>{session?.user?.introduction}</CardDescription>
+          </CardHeader>
+          <CardContent className="grid w-full items-center gap-4">
+            <Avatar>
+              <AvatarImage src={session?.user?.image || ""} />
+              <AvatarFallback>CN</AvatarFallback>
+            </Avatar>
+          </CardContent>
+        </Card>
+      </div>
     </>
   );
 };
