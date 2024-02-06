62527bd299b699627e6983e10ad23b02ff24863e
udonrm <b201395@hiroshima-u.ac.jp>
2024-02-07 00:01:53 +0900

    本の編集権限の設定

--- src/app/books/[id]/edit/page.tsx
+++ src/app/books/[id]/edit/page.tsx
@@ -1,22 +1,30 @@
 "use client";
 import { Header } from "@/app/components/header";
-import { useParams } from "next/navigation";
+import { useRouter, useParams } from "next/navigation";
 import BookForm from "@/app/components/bookForm";
 import { useEffect, useState } from "react";
 import { Book } from "@/types";
+import { useSession } from "next-auth/react";
 
 const EditBook = () => {
+  const router = useRouter();
   const { id } = useParams();
   const [book, setBook] = useState<Book>();
+  const { data: session } = useSession();
 
   useEffect(() => {
     const fetchSingleBook = async () => {
       const response = await fetch(`http://localhost:3000/api/book/${id}`);
       const data = await response.json();
       setBook(data.bookWithUserName);
+
+      // 取得した本のデータをuseEffect内で直接使って比較
+      if (session?.user?.userId !== data.bookWithUserName.userId) {
+        router.push("/books");
+      }
     };
     fetchSingleBook();
-  }, [id]);
+  }, [id, book?.userId, session?.user?.userId, router]);
 
   return (
     <>
